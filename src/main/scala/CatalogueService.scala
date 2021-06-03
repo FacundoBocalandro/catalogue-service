2e07cd0a65c79712ad4f29fbd0a92e9d62e5ebaf
Facundo Bocalandro <facundo@bocalandro.com.ar>
2021-06-02 23:55:40 -0300

    Kubernetes integration

--- src/main/scala/CatalogueService.scala
+++ src/main/scala/CatalogueService.scala
@@ -25,10 +25,7 @@ class CSVReader extends LocationDatabase {
   val data: List[Product] = getProducts("catalogue.csv")
 
   override def getProductsByCountry(country: String): List[Product] = {
-    println("data: " + data)
-    val asd = data.filter(prod => prod.country.equals(country))
-    println("asd: " + asd)
-    asd
+    data.filter(prod => prod.country.equals(country))
   }
 }
 
@@ -37,6 +34,7 @@ object CSVReader {
   def getProducts(filePath: String): List[Product] = {
     val fileSource = Source.fromInputStream(getClass.getClassLoader.getResourceAsStream(filePath))
     val data: List[List[String]] = fileSource.getLines().toList.map(_.split(',').toList)
+    println(data)
     fileSource.close()
     data.map {
       case List(id, name, price, country) =>
@@ -47,7 +45,7 @@ object CSVReader {
 }
 
 object CatalogueServiceServer extends App {
-  val builder = ServerBuilder.forPort(sys.env.getOrElse("port", "5000").toInt)
+  val builder = ServerBuilder.forPort(sys.env.getOrElse("port", "50001").toInt)
 
   builder.addService(
     CatalogueService.bindService(new MyService(), ExecutionContext.global)
